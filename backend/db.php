656231205102eb3b8540dc1fc00ba2e2ae19807a
Daniel <gildresdaniel1@gmail.com>
2026-05-15 21:19:42 +0200

    intent chat

--- backend/db.php
+++ backend/db.php
@@ -2,15 +2,17 @@
 
 session_start();
 
-$conn = new mysqli("127.0.0.1", "root", "", "tictactoe");
+$conn = new mysqli(
+ "projectedani.infinityfreeapp.comm",
+ "if0_41931698",
+ "mamadeocallao",
+ "if0_41931698_tictactoe"
+);
 
 if ($conn->connect_error) {
-    http_response_code(500);
-    header("Content-Type: application/json");
-    echo json_encode(["error" => "DB error"]);
-    exit;
+ die("Error: " . $conn->connect_error);
 }
 
-$conn->set_charset("utf8mb4");
 
 header("Content-Type: application/json");
+
